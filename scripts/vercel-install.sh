98af340cc0fea512670f150aea697a8f1e7bf2b6
zhenyan121 <104683324+zhenyan121@users.noreply.github.com>
2026-09-09 17:12:47 +0800

    chore(build): switch Trunk to musl target for Vercel

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -3,8 +3,9 @@
 set -eu
 
 trunk_version="0.21.14"
-trunk_sha256="f2b4680cd239693a646a2795e4633c625328d7b2a044fbe749fa3a2fe9e7036b"
-trunk_dir=".vercel/cache/trunk/${trunk_version}"
+trunk_target="x86_64-unknown-linux-musl"
+trunk_sha256="a67f4054b249fe9acc5fabc25de1aebf19783aca3ad6ff64bf34d7da44d0ea20"
+trunk_dir=".vercel/cache/trunk/${trunk_version}/${trunk_target}"
 trunk_bin="${trunk_dir}/trunk"
 
 rustup target add wasm32-unknown-unknown
@@ -19,7 +20,7 @@ trap 'rm -rf "${archive_dir}"' EXIT HUP INT TERM
 archive="${archive_dir}/trunk.tar.gz"
 
 curl --fail --location --retry 3 --silent --show-error \
-    "https://github.com/trunk-rs/trunk/releases/download/v${trunk_version}/trunk-x86_64-unknown-linux-gnu.tar.gz" \
+    "https://github.com/trunk-rs/trunk/releases/download/v${trunk_version}/trunk-${trunk_target}.tar.gz" \
     --output "${archive}"
 
 echo "${trunk_sha256}  ${archive}" | sha256sum --check --status
